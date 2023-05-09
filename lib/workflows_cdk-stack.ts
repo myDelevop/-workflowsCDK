847fd96ac5b825d7c3949044adc8a0445adc4a71
Rocco Caliandro <rocco.caliandro@hotmail.com>
2023-05-09 17:14:47 +0200

    added lambda function definition

--- lib/workflows_cdk-stack.ts
+++ lib/workflows_cdk-stack.ts
@@ -1,13 +1,20 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import { DynamoDbDataSource } from 'aws-cdk-lib/aws-appsync';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class WorkflowsCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    const myTablr = new ddb.Table(this, 'MyTable', 
+
+    const myTable = new ddb.Table(this, 'MyTable', 
       {partitionKey: {name: "RequestId", type: ddb.AttributeType.STRING}});
+
+    const submitLambda = new lambda.Function(this, 'submitLambda', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      handler: 'index.main',
+      code: lambda.Code.fromInline('def main(event, context):\n\treturn(event)')
+    });
 
 
 /*const queue = new sqs.Queue(this, 'WorkflowsCdkQueue', {
